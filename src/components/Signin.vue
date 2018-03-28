98da713af3a29cd61efa2e21e4a63795b28d7815
david chapman <chap6680@gmail.com>
2018-03-28 02:23:47 -0600

    firebase user start

---
+++ src/components/Signin.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="from-inline">
+	<h3>Sign In</h3>
+	<div class="form-group"></div>
+	<input 
+	type = "text"
+	placeholder="email"
+	class="form-control"
+	v-model="email"
+	/>
+	<input
+	type = "password"
+	placeholder="password"
+	class="form-control"
+	v-model="password"
+	/>
+	<button type="btn btn-primary">Sign In </button>
+</div>
+</template>
+<script>
+export default {
+  data(){
+	  return{
+		  email:'',
+		  password:''
+	  }
+  }
+}
+</script>
---
+++ src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyA8ouwUf-dzpJ3rbYd4oZ5V_o4ZwkaCRWE",
+    authDomain: "maxmeetup-251d0.firebaseapp.com",
+    databaseURL: "https://maxmeetup-251d0.firebaseio.com",
+    projectId: "maxmeetup-251d0",
+    storageBucket: "maxmeetup-251d0.appspot.com",
+    messagingSenderId: "917133124294"
+  };
+
+  export const firebaseApp = firebase.initializeApp(config)
