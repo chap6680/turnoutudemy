f15dd44fa0f622a1e95631b557b1d38082257c92
david chapman <chap6680@gmail.com>
2018-03-31 13:46:51 -0600

    added ability to create record in firebase

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,19 @@
 <template>
 <div class="">
-	<h3>Dashboard Componentx</h3>
+	<h3>Dashboard Component</h3>
 	<button class="btn btn-danger btn-sm btn signout-btn" @click="signOut">SignOut</button>
+	<hr>
+	<AddEvent/>
+	<hr>
 	{{$store.state}}
+	
+
 </div>
 </template>
 
 <script>
 import {firebaseApp} from '../firebaseApp'
+import AddEvent from './AddEvent.vue'
 
 export default {
 	methods: {
@@ -15,6 +21,9 @@ export default {
 			this.$store.dispatch('signOUt')
 			firebaseApp.auth().signOut()
 		}
+	},
+	components: {
+		AddEvent
 	}
 }
 </script>
